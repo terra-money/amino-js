f0f478dfa92295158d44a3d9fab50a022c5ad29c
yys <sw.yunsuk@gmail.com>
2020-10-07 15:14:38 +0900

    update for columbus-4 msgs

--- go/extensions/codec.go
+++ go/extensions/codec.go
@@ -7,9 +7,18 @@ import (
 func RegisterCodec(codec *amino.Codec) {
 	codec.RegisterConcrete(MsgExchangeRateVote{}, TerraMsgExchangeRateVote, nil)
 	codec.RegisterConcrete(MsgExchangeRatePrevote{}, TerraMsgExchangeRatePrevote, nil)
+	codec.RegisterConcrete(MsgAggregateExchangeRateVote{}, TerraMsgAggregateExchangeRateVote, nil)
+	codec.RegisterConcrete(MsgAggregateExchangeRatePrevote{}, TerraMsgAggregateExchangeRatePrevote, nil)
 	codec.RegisterConcrete(MsgDelegateFeedConsent{}, TerraMsgDelegateFeedConsent, nil)
 
 	codec.RegisterConcrete(MsgSwap{}, TerraMsgSwap, nil)
+	codec.RegisterConcrete(MsgSwapSend{}, TerraMsgSwapSend, nil)
+
+	codec.RegisterConcrete(MsgStoreCode{}, TerraMsgStoreCode, nil)
+	codec.RegisterConcrete(MsgInstantiateContract{}, TerraMsgInstantiateContract, nil)
+	codec.RegisterConcrete(MsgExecuteContract{}, TerraMsgExecuteContract, nil)
+	codec.RegisterConcrete(MsgMigrateContract{}, TerraMsgMigrateContract, nil)
+	codec.RegisterConcrete(MsgUpdateContractOwner{}, TerraMsgUpdateContractOwner, nil)
 
 	codec.RegisterConcrete(&Schedule{}, TerraSchedule, nil)
 	codec.RegisterConcrete(&VestingSchedule{}, TerraVestingSchedule, nil)
--- go/extensions/types.go
+++ go/extensions/types.go
@@ -6,9 +6,17 @@ import (
 )
 
 var _ sdk.Msg = (*MsgSwap)(nil)
+var _ sdk.Msg = (*MsgSwapSend)(nil)
 var _ sdk.Msg = (*MsgExchangeRatePrevote)(nil)
 var _ sdk.Msg = (*MsgExchangeRateVote)(nil)
+var _ sdk.Msg = (*MsgAggregateExchangeRatePrevote)(nil)
+var _ sdk.Msg = (*MsgAggregateExchangeRateVote)(nil)
 var _ sdk.Msg = (*MsgDelegateFeedConsent)(nil)
+var _ sdk.Msg = (*MsgStoreCode)(nil)
+var _ sdk.Msg = (*MsgInstantiateContract)(nil)
+var _ sdk.Msg = (*MsgExecuteContract)(nil)
+var _ sdk.Msg = (*MsgMigrateContract)(nil)
+var _ sdk.Msg = (*MsgUpdateContractOwner)(nil)
 
 type Schedule struct {
 	StartTime int64   `json:"start_time"`
@@ -33,6 +41,13 @@ type MsgSwap struct {
 	AskDenom  string         `json:"ask_denom"`  // Denom of the coin to swap to
 }
 
+type MsgSwapSend struct {
+	FromAddress sdk.AccAddress `json:"from_address" yaml:"from_address"` // Address of the offer coin payer
+	ToAddress   sdk.AccAddress `json:"to_address" yaml:"to_address"`     // Address of the recipient
+	OfferCoin   sdk.Coin       `json:"offer_coin" yaml:"offer_coin"`     // Coin being offered
+	AskDenom    string         `json:"ask_denom" yaml:"ask_denom"`       // Denom of the coin to swap to
+}
+
 type MsgExchangeRatePrevote struct {
 	Hash      string         `json:"hash"` // hex string
 	Denom     string         `json:"denom"`
@@ -48,17 +63,72 @@ type MsgExchangeRateVote struct {
 	Validator    sdk.ValAddress `json:"validator"`
 }
 
+type MsgAggregateExchangeRatePrevote struct {
+	Hash      string         `json:"hash" yaml:"hash"`
+	Feeder    sdk.AccAddress `json:"feeder" yaml:"feeder"`
+	Validator sdk.ValAddress `json:"validator" yaml:"validator"`
+}
+
+type MsgAggregateExchangeRateVote struct {
+	Salt          string         `json:"salt" yaml:"salt"`
+	ExchangeRates string         `json:"exchange_rates" yaml:"exchange_rates"` // comma separated dec coins
+	Feeder        sdk.AccAddress `json:"feeder" yaml:"feeder"`
+	Validator     sdk.ValAddress `json:"validator" yaml:"validator"`
+}
+
 type MsgDelegateFeedConsent struct {
 	Operator sdk.ValAddress `json:"operator"`
 	Delegate sdk.AccAddress `json:"delegate"`
 }
 
+type MsgStoreCode struct {
+	Sender sdk.AccAddress `json:"sender" yaml:"sender"`
+	// WASMByteCode can be raw or gzip compressed
+	WASMByteCode string `json:"wasm_byte_code" yaml:"wasm_byte_code"`
+}
+
+type MsgInstantiateContract struct {
+	Owner      sdk.AccAddress `json:"owner" yaml:"owner"`
+	CodeID     uint64         `json:"code_id" yaml:"code_id"`
+	InitMsg    string         `json:"init_msg" yaml:"init_msg"`
+	InitCoins  sdk.Coins      `json:"init_coins" yaml:"init_coins"`
+	Migratable bool           `json:"migratable" yaml:"migratable"`
+}
+
+type MsgExecuteContract struct {
+	Sender     sdk.AccAddress `json:"sender" yaml:"sender"`
+	Contract   sdk.AccAddress `json:"contract" yaml:"contract"`
+	ExecuteMsg string         `json:"execute_msg" yaml:"execute_msg"`
+	Coins      sdk.Coins      `json:"coins" yaml:"coins"`
+}
+
+type MsgMigrateContract struct {
+	Owner      sdk.AccAddress `json:"owner" yaml:"owner"`
+	Contract   sdk.AccAddress `json:"contract" yaml:"contract"`
+	NewCodeID  uint64         `json:"new_code_id" yaml:"new_code_id"`
+	MigrateMsg string         `json:"migrate_msg" yaml:"migrate_msg"`
+}
+
+type MsgUpdateContractOwner struct {
+	Owner    sdk.AccAddress `json:"owner" yaml:"owner"`
+	NewOwner sdk.AccAddress `json:"new_owner" yaml:"new_owner"`
+	Contract sdk.AccAddress `json:"contract" yaml:"contract"`
+}
+
 const (
-	TerraMsgSwap                      = "market/MsgSwap"
-	TerraMsgExchangeRateVote          = "oracle/MsgExchangeRateVote"
-	TerraMsgExchangeRatePrevote       = "oracle/MsgExchangeRatePrevote"
-	TerraMsgDelegateFeedConsent       = "oracle/MsgDelegateFeedConsent"
-	TerraSchedule                     = "core/Schedule"
-	TerraVestingSchedule              = "core/VestingSchedule"
-	TerraBaseLazyGradedVestingAccount = "core/LazyGradedVestingAccount"
+	TerraMsgSwap                         = "market/MsgSwap"
+	TerraMsgSwapSend                     = "market/MsgSwapSend"
+	TerraMsgExchangeRateVote             = "oracle/MsgExchangeRateVote"
+	TerraMsgExchangeRatePrevote          = "oracle/MsgExchangeRatePrevote"
+	TerraMsgDelegateFeedConsent          = "oracle/MsgDelegateFeedConsent"
+	TerraMsgAggregateExchangeRatePrevote = "oracle/MsgAggregateExchangeRatePrevote"
+	TerraMsgAggregateExchangeRateVote    = "oracle/MsgAggregateExchangeRateVote"
+	TerraSchedule                        = "core/Schedule"
+	TerraVestingSchedule                 = "core/VestingSchedule"
+	TerraBaseLazyGradedVestingAccount    = "core/LazyGradedVestingAccount"
+	TerraMsgStoreCode                    = "wasm/MsgStoreCode"
+	TerraMsgInstantiateContract          = "wasm/MsgInstantiateContract"
+	TerraMsgExecuteContract              = "wasm/MsgExecuteContract"
+	TerraMsgMigrateContract              = "wasm/MsgMigrateContract"
+	TerraMsgUpdateContractOwner          = "wasm/MsgUpdateContractOwner"
 )
